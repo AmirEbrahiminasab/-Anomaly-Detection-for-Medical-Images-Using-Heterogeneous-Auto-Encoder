030a6dfad105a00f8d83b4f62ecd1490b99ee674
AmirEbrahiminasab <ebrahiminasab82@gmail.com>
2025-08-12 21:40:29 +0330

    updated main.py

--- scripts/main.py
+++ scripts/main.py
@@ -1,22 +1,11 @@
 import torch
-import torch.nn as nn
-from torch.utils.data import Dataset, DataLoader
-from torchvision import transforms
-from PIL import Image
 import os
-import glob
 import numpy as np
-from tqdm import tqdm
-import torch.nn.functional as F
-from torchvision import models
-from typing import List, Dict
-import math
 import sys
 
 np.random.seed(42)
 import train
-import evaluate
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+# sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 import models
 from data import data_loader
 from utils import visualization, metrics
