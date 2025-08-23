9ddcdbe2a65e812f66aa39e3e684fe3a9cd767e2
amirhoseinnsy <nasrya132amn@gmail.com>
2025-08-23 13:16:43 +0330

    Update hyperparameter tunning and train with best hyperparameters

--- scripts/main.py
+++ scripts/main.py
@@ -4,8 +4,12 @@ import numpy as np
 import sys
 import random
 import pandas as pd
+from torch.utils.data import DataLoader
+from typing import Tuple
 
 np.random.seed(42)
+# random.seed(42)
+
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from scripts.train import train
 import models
@@ -28,11 +32,15 @@ def chest_xray():
     print(f"Using device: {device}")
     print(torch.__version__)
 
+    LEARNING_RATE = 0.0003
+    ALPHA_LOSS = 0.7
+    BATCH_SIZE = 32
+
     train_loader, test_normal_loader, test_abnormal_loader = get_dataloader_chest_xray(INPUT_SIZE, BATCH_SIZE)
 
     best_model = train(
         train_loader, test_normal_loader, test_abnormal_loader,
-        EPOCHS, device, ALPHA_LOSS, LEARNING_RATE, INPUT_SIZE, patience=500
+        EPOCHS, device, ALPHA_LOSS, LEARNING_RATE, INPUT_SIZE, patience=10
     )
 
     SAVE_DIR = 'models/saved_models'
@@ -57,6 +65,10 @@ def brain_tumor():
     print(f"Using device: {device}")
     print(torch.__version__)
 
+    LEARNING_RATE = 3e-05
+    ALPHA_LOSS = 0.5
+    BATCH_SIZE = 32
+
     train_loader, test_normal_loader, test_abnormal_loader = get_dataloader_brain_tumor(
         dataset_root=DATA_DIR,
         input_size=INPUT_SIZE,
@@ -65,7 +77,7 @@ def brain_tumor():
 
     best_model = train(
         train_loader, test_normal_loader, test_abnormal_loader,
-        EPOCHS, device, ALPHA_LOSS, LEARNING_RATE, INPUT_SIZE, patience=500
+        EPOCHS, device, ALPHA_LOSS, LEARNING_RATE, INPUT_SIZE, patience=10
     )
 
     SAVE_DIR = 'models/saved_models'
@@ -96,6 +108,9 @@ def covid19():
     print(f"Using device: {device}")
     print(torch.__version__)
 
+    LEARNING_RATE = 0.003
+    ALPHA_LOSS = 0.5
+    BATCH_SIZE = 32
     train_loader, test_normal_loader, test_abnormal_loader = get_dataloader_covid19(
         dataset_root=DATA_DIR,
         input_size=INPUT_SIZE,
@@ -105,7 +120,7 @@ def covid19():
 
     best_model = train(
         train_loader, test_normal_loader, test_abnormal_loader,
-        EPOCHS, device, ALPHA_LOSS, LEARNING_RATE, INPUT_SIZE, patience=500
+        EPOCHS, device, ALPHA_LOSS, LEARNING_RATE, INPUT_SIZE, patience=10
     )
 
     SAVE_DIR = 'models/saved_models'
@@ -163,22 +178,25 @@ def oct2017():
     )
 
 
-def hyperparameter_tuning_random_search(num_trials, train_loader, test_normal_loader, test_abnormal_loader, device, input_size, dataset_name):
+def hyperparameter_tuning_random_search(num_trials, device, input_size, dataset_name):
     results = []
 
-    learning_rate_space = [1e-3, 1e-4, 5e-5, 1e-5]
-    alpha_loss_space = [0.5, 0.6, 0.7, 0.8]
+    learning_rate_space = [3e-3, 3e-4, 5e-5, 3e-5]
+    alpha_loss_space = [0.3, 0.5, 0.7]
     batch_size_space = [32, 64, 128, 200]
+
     
     tuning_epochs = 10
 
     for i in range(num_trials):
         learning_rate = random.choice(learning_rate_space)
         alpha_loss = random.choice(alpha_loss_space)
-        batch_size = random.choice(batch_size_space)
+        batch_size = random.choice(batch_size_space) 
 
         print(f"--- Trial {i+1}/{num_trials} ---")
         print(f"Hyperparameters: LR={learning_rate}, Alpha={alpha_loss}, Batch Size={batch_size}")
+
+        train_loader, test_normal_loader, test_abnormal_loader = data_loader_all(dataset_name, batch_size)
 
         best_model = train(
             train_loader, test_normal_loader, test_abnormal_loader,
@@ -203,90 +221,81 @@ def hyperparameter_tuning_random_search(num_trials, train_loader, test_normal_lo
         results.append(trial_results)
 
         df = pd.DataFrame(results)
-        df.to_csv('hyperparameter_tuning_results.csv', index=False)
-        print(f"Results for trial {i+1} saved to hyperparameter_tuning_results_{dataset_name}.csv")
+        df.to_csv(f'hyperparameter_tuning_results_{dataset_name}.csv', index=False)
+        print(f"Results for trial {i+1} saved to hyperparameter_tuning_results.csv")
 
     print("\n--- Hyperparameter Tuning Complete ---")
-    print(pd.read_csv('hyperparameter_tuning_results.csv'))
+    print(pd.read_csv(f'hyperparameter_tuning_results_{dataset_name}.csv'))
 
 
 def tunning():
-    DATA_DIR = "/home/appliedailab/Desktop/Deep/brain_tumor"
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     print(f"Using device: {device}")
     print(torch.__version__)
 
-    train_loader, test_normal_loader, test_abnormal_loader = get_dataloader_brain_tumor(
+    num_trials = 10
+    
+    hyperparameter_tuning_random_search(
+        num_trials=num_trials,
+        device=device,
+        input_size=INPUT_SIZE,
+        dataset_name='brain'
+    )
+
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    print(f"Using device: {device}")
+    print(torch.__version__)
+
+    num_trials = 10
+    
+    hyperparameter_tuning_random_search(
+        num_trials=num_trials,
+        device=device,
+        input_size=INPUT_SIZE,
+        dataset_name='xray'
+    )
+
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    print(f"Using device: {device}")
+    print(torch.__version__)
+
+    num_trials = 10
+    
+    hyperparameter_tuning_random_search(
+        num_trials=num_trials,
+        device=device,
+        input_size=INPUT_SIZE,
+        dataset_name='covid'
+    )
+
+
+def data_loader_all(dataset_name:str, BATCH_SIZE):
+    if dataset_name == 'brain':
+        DATA_DIR = "/home/appliedailab/Desktop/Deep/brain_tumor"
+        train_loader, test_normal_loader, test_abnormal_loader = get_dataloader_brain_tumor(
         dataset_root=DATA_DIR,
         input_size=INPUT_SIZE,
         batch_size=BATCH_SIZE, # This will be effectively overridden in the tuning loop
     )
+    elif dataset_name == 'xray':
+        DATA_DIR = 'chest_xray/chest_xray'
+        train_loader, test_normal_loader, test_abnormal_loader = get_dataloader_chest_xray(INPUT_SIZE, BATCH_SIZE)
+    elif dataset_name == 'covid':
+        DATA_DIR = "/home/appliedailab/Desktop/Deep/CovidDataset"
+        train_loader, test_normal_loader, test_abnormal_loader = get_dataloader_covid19(
+            dataset_root=DATA_DIR,
+            input_size=INPUT_SIZE,
+            batch_size=BATCH_SIZE,
+            download_url="https://figshare.com/ndownloader/files/50920287",  # used only if folder missing
+        )
 
-
-    num_trials = 10
-    
-    hyperparameter_tuning_random_search(
-        num_trials=num_trials,
-        train_loader=train_loader,
-        test_normal_loader=test_normal_loader,
-        test_abnormal_loader=test_abnormal_loader,
-        device=device,
-        input_size=INPUT_SIZE,
-        dataset_name="brain_tumor"
-    )
-
-    DATA_DIR = 'chest_xray/chest_xray'
-
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    print(f"Using device: {device}")
-    print(torch.__version__)
-
-    train_loader, test_normal_loader, test_abnormal_loader = get_dataloader_chest_xray(INPUT_SIZE, BATCH_SIZE)
-
-    num_trials = 10
-    
-    hyperparameter_tuning_random_search(
-        num_trials=num_trials,
-        train_loader=train_loader,
-        test_normal_loader=test_normal_loader,
-        test_abnormal_loader=test_abnormal_loader,
-        device=device,
-        input_size=INPUT_SIZE,
-        dataset_name="chest_xray"
-    )
-
-    DATA_DIR = "/home/appliedailab/Desktop/Deep/CovidDataset"
-
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    print(f"Using device: {device}")
-    print(torch.__version__)
-
-    train_loader, test_normal_loader, test_abnormal_loader = get_dataloader_covid19(
-        dataset_root=DATA_DIR,
-        input_size=INPUT_SIZE,
-        batch_size=BATCH_SIZE,
-        download_url="https://figshare.com/ndownloader/files/50920287",  # used only if folder missing
-    )
-
-    num_trials = 10
-    
-    hyperparameter_tuning_random_search(
-        num_trials=num_trials,
-        train_loader=train_loader,
-        test_normal_loader=test_normal_loader,
-        test_abnormal_loader=test_abnormal_loader,
-        device=device,
-        input_size=INPUT_SIZE,
-        dataset_name="covid"
-    )
-
-
+    return train_loader, test_normal_loader, test_abnormal_loader
 
     
 if __name__ == "__main__":
     # chest_xray()
-    brain_tumor()
-    # covid19()
+    # brain_tumor()
+    covid19()
     # oct2017()
     # tunning()
 
